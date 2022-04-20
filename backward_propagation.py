fixed vanishing gradient :)

backward_propagation.py
@@ -1,4 +1,8 @@
+"""
+Performs backward propagation
+"""
 import numpy as np
+
 
 def linear_backward(dZ, cache):
     """
@@ -11,15 +15,12 @@ def linear_backward(dZ, cache):
         db -- Gradient of the cost with respect to b (current layer l), same shape as b
     """
     m = cache['A'].shape[1]
-    # dW_curr = np.dot(dZ,cache['A'].transpose())/m# np.dot(cache['A'].transpose(), dZ) / m #TODO
-    # db_curr = np.sum(dZ, axis=0, keepdims=True) / m
-    # dA_prev = np.dot(dZ, cache['W'].transpose())
 
-    dA_prev = np.matmul(cache['W'].transpose(), dZ) #.transpose()
-    dW_curr = np.matmul(dZ, cache['A'].transpose())/m
-    db_curr =  np.sum(dZ, axis=1)/m
-    db_curr = db_curr.reshape(-1, 1)
+    dA_prev = np.matmul(cache['W'].transpose(), dZ)
+    dW_curr = np.matmul(dZ, cache['A'].transpose()) / m
+    db_curr = np.sum(dZ, axis=1) / m
 
+    db_curr = np.expand_dims(db_curr, axis=1)
 
     return dA_prev, dW_curr, db_curr
 
@@ -47,9 +48,10 @@ def linear_activation_backward(dA, cache, activation):
     else:
         activation_function = softmax_backward
 
-    dz = activation_function(dA, cache) # TODO how to send the right cache?
+    dz = activation_function(dA, cache)
     dA_prev, dW, db = linear_backward(dz, cache)
     return dA_prev, dW, db
+
 
 def relu_backward(dA, activation_cache):
     """
@@ -64,7 +66,6 @@ def relu_backward(dA, activation_cache):
     # When z <= 0, you should set dz to 0 as well.
     dZ[Z <= 0] = 0
     return dZ
-
 
 
 def softmax_backward(dA, activation_cache):
@@ -99,14 +100,15 @@ def L_model_backward(AL, Y, caches):
     grads = {}
     dA_prev = None
     num_of_layers = len(caches)
-    caches[num_of_layers -1 ]['AL'] = AL
+    caches[num_of_layers - 1]['AL'] = AL
     caches[num_of_layers - 1]['Y'] = Y
     for layer in range(num_of_layers, 0, -1):
         activation = 'relu' if dA_prev is not None else 'softmax'
-        dA = dA_prev if dA_prev is not None else -(Y/AL)+(1-Y)/(1-AL)
+        dA = dA_prev if dA_prev is not None else -(Y / AL) + (1 - Y) / (1 - AL)
         dA_prev, dW, db = linear_activation_backward(dA, caches[layer - 1], activation)
         grads.update({f'dA{layer}': dA, f'dW{layer}': dW, f'db{layer}': db})
     return grads
+
 
 def update_parameters(parameters, grads, learning_rate):
     """

main.py
@@ -1,19 +1,48 @@
 from tensorflow.keras.datasets.mnist import load_data
 import build_network
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_cost_func(costs):
+    """
+    Plot the cost over training steps graph
+    :param costs: list of all costs
+    """
+    plt.plot(costs)
+    plt.xlabel("Training step")
+    plt.ylabel("Cost")
+    plt.title("Validation costs")
+    plt.legend()
+    plt.show()
 
 
 def normalise_data(x, y):
-    # x_normalized = np.array([x.flatten()/255.0 for x in x])
-    x_normalized = x.reshape(x.shape[0], x.shape[1] * x.shape[2]) / 255.0
-    # y_flatten = np.expand_dims(y_train.transpose(), axis=1)
-    # y_flatten = y_flatten.flatten().astype(int)
-    y_normalized = np.zeros((y.shape[0], 10))
+    """
+    performs reshape & normalization to the mnist data
+    :param x: mnist digits data
+    :param y: mnist digits labels
+    :return:
+    x_normalized, y_normalized - prepared data to run
+    """
+    num_of_classes = 10
+    pixel_value = 255.0
+    x_normalized = np.array([x.flatten() / pixel_value for x in x])
+    y = np.expand_dims(y.transpose(), axis=1)
+    y = y.flatten().astype(int)
+    y_normalized = np.zeros((y.shape[0], num_of_classes))
     y_normalized[np.arange(y.shape[0]), y] = 1
+
     return x_normalized, y_normalized
 
 
 def prep_data():
+    """
+    loads and splits the mnist data into train & test subsets,
+    then prepares the data for the neural network
+    :return:
+    x_train, y_train, x_test, y_test - data to run
+    """
     (x_train, y_train), (x_test, y_test) = load_data(path='mnist.npz')
     x_train, y_train = normalise_data(x_train, y_train)
     x_test, y_test = normalise_data(x_test, y_test)
@@ -21,27 +50,11 @@ def prep_data():
     return x_train, y_train, x_test, y_test
 
 
-"""
-4.	Use the code you wrote to classify the MNIST dataset and present a summary report
-    a.	Load the dataset using the Keras code. Note that there is a predefined division between the train and test set.
-        Use 20% of the training set as a validation set (samples need to be randomly chosen).
-    b.	Run your network using the following configuration:
-        •	4 layers (aside from the input layer), with the following sizes: 20,7,5,10
-        •	Do not activate the batchnorm option at this point
-        •	The input at each iteration needs to be “flattened” to a matrix of [m,784], where m is the number of samples
-        •	Use a learning rate of 0.009
-        •	Train the network until there is no improvement on the validation set (or the improvement is very small)
-            for 100 training steps (this is the stopping criterion). Please include in the report the number of iterations
-            and epochs needed to train your network. Also, specify the batch size.
-    c.	Please include the following details in your report:
-        •	The final accuracy values for the train, validation and test sets.
-        •	The cost value for each 100 training steps. Please make sure that the index of the training step will
-            also be included in the report. Print the values from the L_layer_model
-    d.	All the information requested above will be included in a .docx file uploaded with the code.
-"""
 if __name__ == '__main__':
+    np.random.seed(33)
     x_train, y_train, x_test, y_test = prep_data()
     parameters, costs = build_network.L_layer_model(x_train.T, y_train.T, layers_dims=[784, 20, 7, 5, 10],
                                                     learning_rate=0.009,
-                                                    num_iterations=100, batch_size=64)
+                                                    num_iterations=10, batch_size=64)
     print("ACC: ", build_network.predict(x_test.T, y_test.T, parameters))
+    plot_cost_func(costs)